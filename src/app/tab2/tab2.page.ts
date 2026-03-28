7b1bc9279d9761c7bdcdee73e1e7588a9ee3ab13
Thelan Nguyen <thelannguyen5@gmail.com>
2026-03-28 18:54:37 +0100

    séries/films liste de liste et scroll horizontal youhou

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -12,6 +12,11 @@ export class Tab2Page {
   myForm: FormGroup;
 
   switchPage: boolean = true; //true = séries, false = films
+  test = [
+    { titre: 'pipi', items: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20] },
+    { titre: 'caca', items: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20] },
+    { titre: 'prout', items: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20] }
+  ];
 
   constructor(private fb: FormBuilder, private cdr: ChangeDetectorRef) {
     this.myForm = this.fb.group({ text: [''] });
