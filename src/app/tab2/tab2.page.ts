fcc5126096e79262ce3c013ac978a8c0a1628a89
Thelan Nguyen <thelannguyen5@gmail.com>
2026-03-28 19:44:17 +0100

    liste films implémenté avec les services watched et added

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,7 @@
 import {ChangeDetectorRef, Component} from '@angular/core';
 import {FormBuilder, FormGroup} from "@angular/forms";
+import { WatchedService } from '../service/watched.service';
+import { AddedService} from "../service/added.service";
 
 @Component({
   selector: 'app-tab2',
@@ -18,9 +20,24 @@ export class Tab2Page {
     { titre: 'prout', items: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20] }
   ];
 
-  constructor(private fb: FormBuilder, private cdr: ChangeDetectorRef) {
+  filmsVus: any[] = [];
+  filmsAjoute: any[] = [];
+
+  constructor(
+    private fb: FormBuilder,
+    private cdr: ChangeDetectorRef,
+    private watchedService: WatchedService,
+    private addedService: AddedService
+  ) {
     this.myForm = this.fb.group({ text: [''] });
   }
+
+  //Chargement des données à chaque fois qu'onarrive sur la page
+  ionViewWillEnter() {
+    this.filmsVus = this.watchedService.getWatched();
+    this.filmsAjoute = this.addedService.getAdded();
+  }
+
   onRecherche() {
     this.recherche = this.myForm.value.text;
     this.cdr.detectChanges();
